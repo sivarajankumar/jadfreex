60d2660b7a55e01282bad91c961d35513eac4ff2
jadfreex@gmail.com <jadfreex@gmail.com@824876ea-9924-b63d-a48e-dc8a3da11bf1>
2013-11-27 20:58:37 +0000

    Cambios en el Main

--- jadfreex-swing-view/trunk/src/main/java/net/jadfreex/swingview/usuario/Main.java
+++ jadfreex-swing-view/trunk/src/main/java/net/jadfreex/swingview/usuario/Main.java
@@ -3,14 +3,18 @@ package net.jadfreex.swingview.usuario;
 import net.jadfreex.swingview.commons.AppContext;
 
 public class Main {
-	
+
 	Main() {
 		AppContext.load(this, "classpath:app-config.xml");
+	}
+
+	public void sayHi() {
 		System.out.println("Ya estuvo?");
 	}
-	
+
 	public static void main(String args[]) {
 		Main m = new Main();
+		m.sayHi();
 	}
 
 }
