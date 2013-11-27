3b006473c0cf8e05417143347868acf37c005fb4
jadfreex <jadfreex@gmail.com>
2013-11-27 23:11:12 +0000

    Cambios:
    *Se agrego un bean para indicar el tipo de usuario.
    *Se hizo la relacion entre usuario y tipo de usuario.
    *Se agrego el dao base para usuario.

---
+++ jadfreex-usuario/trunk/src/main/java/net/jadfreex/usuario/dao/UsuarioDAO.java
@@ -0,0 +1,7 @@
+package net.jadfreex.usuario.dao;
+
+import net.jadfreex.commons.dao.GenericDao;
+import net.jadfreex.usuario.bean.Usuario;
+
+public interface UsuarioDAO extends GenericDao<Usuario, Long> {
+}
