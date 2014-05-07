3719f81a21b422e35ceef8205bfeda344b2cdb2e
jadfreex <jadfreex@824876ea-9924-b63d-a48e-dc8a3da11bf1>
2014-05-07 21:29:37 +0000

    Primer commit :D

---
+++ front-angular/trunk/WebContent/js/map.js
@@ -0,0 +1,42 @@
+function Map($scope, $http) {
+	//Declaración de variables
+    var
+	mapa = Raphael('mapa', 800, 450),
+	attr = {
+			'stroke-width': 1,
+			'stroke': 'green',
+			'stroke-linejoin':'round'
+	},
+    path = null;
+    
+	mapa.safari();
+	
+	//GET
+    $http({method: 'GET', url: 'http://localhost:8080/test-app/rest/map/list'}).
+        success(function(paths) {
+        	//$scope.greeting = data;
+        	paths.forEach(function(estado) {
+    	        path = mapa.path(estado.path);
+    			attr.fill = estado.color;
+    			path.attr(attr);
+    	        path.hover(
+    				function(){
+    					this.animate({
+    						fill: 'white',
+    					}, 100);
+    				},
+    				function(){
+    					this.animate({
+    						fill: estado.color
+    					}, 100);
+    				}
+    			);
+    			path.click(function(){
+    				Info($scope, $http, estado.id);
+    	        });
+    		});
+        }).
+        error(function(){
+        	alert("Josefuck dice: Tu aplicación esta mal! Pinche chavo! ¬¬");
+        });
+}
